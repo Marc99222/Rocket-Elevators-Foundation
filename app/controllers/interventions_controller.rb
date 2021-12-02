056ab202de2d11068d55211c5394189bc90f6fc8
Marc99222 <91087569+Marc99222@users.noreply.github.com>
2021-12-01 22:26:44 -0500

    Zendesk v1 are created, not working

--- app/controllers/interventions_controller.rb
+++ app/controllers/interventions_controller.rb
@@ -1,35 +1,36 @@
+require 'zendesk_main'
+
 class InterventionsController < ApplicationController
-  #  before_action :correct_user
+ 
   def index
     @interventions = Intervention.new()
   end
 
 
-  # def set_current_user
-  #   @author = current_user
-  #   if session[:user_id]
-  #     Current.user = User.find_by(id: session[:user_id])
 
 
   def create 
-   
-  # 2 @author.correct_user()
-  
-    
-
     @interventions = Intervention.new(interventions_params)
     @interventions.author = current_user.id
     @interventions.result = 'Incomplete'
     @interventions.status =  'Pending'
     @interventions.start_at =  nil
     @interventions.end_at =  nil
+    ZendeskAPI::Ticket.create!($client, :subject => "Task", :requester => { :name => @interventions.company_name, :email => "marcapc@outlook.com" }, :comment => { :body => "Subject: #{@interventions.company_name}
+
+    Comment: The company #{@interventions.company_name} has made a interventon for a #{@interventions.building_id} building and wants the #{@interventions.plan} service. 
     
+    interventions informations: 
+    #{@interventions.battery_id}
+    #{@interventions.column_id}
+    #{@interventions.elevator_id}
+    #{@interventions.elevator_id}
     
+  
+    The company #{@interventions.company_name} can be reached at #{@interventions.email}." }, :priority => "urgent")
 
+  
 
-
-
-    # puts(@intervention.author)
     puts("***************************************************#{@interventions.author}")
     puts("***************************************************#{@interventions.result}")
     puts("***************************************************#{@interventions.status}")
@@ -44,68 +45,7 @@ class InterventionsController < ApplicationController
   redirect_to "intervention/#index"
 
 
-
   end 
-
-
-
-  # def correct_user
-  #   @interventions = current_user.interventions.find_by(id: params[:author])
-  #   redirect_to root_url if @interventions.nil?
-  # end
-
-
-
-
-
-
-
-  # def create
-  #   @quote = Quote.new(quote_params)
-  #   quote_price = @quote.price
-  #   quote_elevator_needed = @quote.elevator_needed
-  #   price_per_elevator = (quote_price.to_f / quote_elevator_needed.to_f)
-  #   ZendeskAPI::Ticket.create!($client, :subject => "Task", :requester => { :name => @quote.company_name, :email => "rocketeam1234@gmail.com" }, :comment => { :body => "Subject: #{@quote.company_name}
-
-  #     Comment: The company #{@quote.company_name} has made a quote for a #{@quote.building_type} building and wants the #{@quote.plan} service. 
-      
-  #     quote informations: 
-  #     #{@quote.elevator_needed}
-  #     #{price_per_elevator.round(2)}
-  #     #{@quote.price}
-  #     #{@quote.fees}
-  #     #{@quote.total_price}
-      
-  #     The company #{@quote.company_name} can be reached at #{@quote.email}." }, :priority => "urgent")
-  #     @quote.save
-  #   redirect_to "#home"
-  # end
-
-
-
-
-
-
-
-  # ZENDESKst
-
-
-
-  # ZendeskAPI::Ticket.create!($client, :subject => "Task", :requester => { :name => @quote.company_name, :email => "rocketeam1234@gmail.com" }, :comment => { :body => "Subject: #{@quote.company_name}
-
-  #   Comment: The company #{@quote.company_name} has made a quote for a #{@quote.building_type} building and wants the #{@quote.plan} service. 
-    
-  #   quote informations: 
-  #   #{@quote.elevator_needed}
-  #   #{price_per_elevator.round(2)}
-  #   #{@quote.price}
-  #   #{@quote.fees}
-  #   #{@quote.total_price}
-    
-  #   The company #{@quote.company_name} can be reached at #{@quote.email}." }, :priority => "urgent")
-
-
-
 
 
 
