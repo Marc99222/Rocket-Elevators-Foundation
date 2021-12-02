9b024e5d5156873fe2e2a4807f4c3ecccd223816
Marc99222 <91087569+Marc99222@users.noreply.github.com>
2021-12-02 14:09:15 -0500

    add set branch main

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ before 'deploy', 'rvm1:install:ruby'
 set :rvm_map_bins, [ 'rake', 'gem', 'bundle', 'ruby', 'puma', 'pumactl' ]
 set :application, "MARC_ALEXANDRE_PERUSSE_CAVANAGH"
 set :repo_url, "https://github.com/Marc99222/Rocket-Elevators-Foundation"
+set :branch, "main"
 # set :branch, ENV['BRANCH'] if ENV['BRANCH']
 
 # Default branch is :main
