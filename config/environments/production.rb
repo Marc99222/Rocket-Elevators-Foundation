e18d74df51413a6c1b2d72e6f4885fd032cc0e1d
Marc99222 <91087569+Marc99222@users.noreply.github.com>
2021-12-10 22:40:20 -0500

    HTTPS and security password are completed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,10 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: "blazer.dokkuapp.com"}
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #in config/environments/production.rb
+  config.to_prepare { Devise::SessionsController.force_ssl }
+  config.to_prepare { Devise::RegistrationsController.force_ssl }
+  
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -48,7 +52,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
